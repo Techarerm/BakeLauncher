3e51d8d429522ff5c0123254c9f95844cb843b8a
TedKai <126402510+Techarerm@users.noreply.github.com>
2024-09-18 18:39:01 +0800

    Add files via upload

--- assets_grabber.py
+++ assets_grabber.py
@@ -86,7 +86,6 @@ def get_assets_index_version(version_data, version_id):
     print("Trying to get assetsIndex version....", color='green')
     asset_index = version_data.get("assetIndex", {})
     asset_index_id = asset_index.get("id")
-    print(os.getcwd())
     if os.getcwd().endswith(version_id):
         assets_index_file = os.path.join('.minecraft', "assets_index.json")
     else:
