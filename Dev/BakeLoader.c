e3e320f9a1a08805de770772c7f0ad912cdac1c4
TedKai <126402510+Techarerm@users.noreply.github.com>
2024-10-01 21:38:22 +0800

    Dev file

---
+++ Dev/BakeLoader.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <direct.h>
+
+int main() {
+    printf("BakeLauncher C Loader :)\n");
+    printf("Version: 1.0.1\n");
+
+    // run main :)
+    if (system("python3 __main__.py") != 0) {
+        perror("Can't start BakeLauncher main!");
+        perror("Maybe your BakeLauncher's file are corrpted :(");
+        return 1;
+    }
+    return 0;
+}
---
+++ Dev/Dev.md
@@ -0,0 +1,14 @@
+Info:
+
+BakeLauncherLoad   // Easily to open non-built BakeLauncher :)
+
+Build(Dependencies: gcc..)
+gcc -o BakeLauncherLoad BakeLoader.c
+
+
+requirements.txt  // BakeLauncher required modules
+
+Usage:
+
+
+
